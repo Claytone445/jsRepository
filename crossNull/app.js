66be4776d82a71e7ecd5a86e8a61c0297c14c899
Claytone445 <brukva90@gmail.com>
2021-04-05 17:19:23 +0300

    CrossNull 0.8.227

---
+++ crossNull/app.js
@@ -0,0 +1,94 @@
+//elements
+const xClass = 'x';
+const circleClass = 'circle';
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.querySelector('#board');
+const winningMessageElement = document.querySelector('#winingMessage');
+const restartButton = document.querySelector('#restartButton');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+
+let circleTurn;
+
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    circleTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove('x');
+        cell.classList.remove('circle');
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, {once: true});
+    });
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+}
+
+function handleClick(e) {
+
+const cell = e.target;
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = 'Ничья!';
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn  ? "Нолики" : "Крестики"} победили!`;
+    }
+    winningMessageElement.classList.add('show');
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains('x') || cell.classList.contains('circle');
+    })
+}
+
+const currentClass = circleTurn ? circleClass : xClass;
+    placeMark(cell, currentClass);
+    if(checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+         endGame(true);
+    } else {
+        swapTurn();
+        setBoardHoverClass();
+    }
+}
+
+function placeMark (cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurn() {
+    circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(xClass);
+    board.classList.remove(circleClass);
+    if (circleTurn) {
+        board.classList.add(circleClass);
+    } else {
+        board.classList.add(xClass);   
+    }
+}
+
+function checkWin(currentClass) {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        })
+    })
+}
